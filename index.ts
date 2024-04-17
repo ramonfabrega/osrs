05e72fcbbb2b31290cbd5d1fd644cd886475a317
Fernando Varela <fernandoavarelaf@gmail.com>
2024-04-18 00:39:15 +0200

    added potions ts. extended sleep to 60 secs and added both scales and potions

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import { checkOnyxArb } from "./onyx";
+import { checkPotionsArbs } from "./potions";
 import { checkScalesArb } from "./zulrah";
 
 while (true) {
+  await checkPotionsArbs();
   await checkScalesArb();
   await sleep();
 }
 
 async function sleep() {
-  await new Promise((resolve) => setTimeout(resolve, 15000));
+  await new Promise((resolve) => setTimeout(resolve, 60000));
   console.log("-----sleep-----");
 }
---
+++ potions.ts
@@ -0,0 +1,35 @@
+import { getLastBuy, getLastSell } from "./wiki";
+
+const IDS = {
+  RESTORES_4: 3024,
+  RESTORES_3: 3026,
+  BREW_4: 6685,
+  BREW_3: 6687,
+  STAMINA_4: 12625,
+  STAMINA_3: 12627,
+};
+
+export async function checkPotionsArbs() {
+  const [brew, restore, stamina] = await Promise.all([
+    checkArbitrage(IDS.BREW_4, IDS.BREW_3, 'Brew'),
+    checkArbitrage(IDS.RESTORES_4, IDS.RESTORES_3, 'Restore'),
+    checkArbitrage(IDS.STAMINA_4, IDS.STAMINA_3, 'Stamina'),
+  ]);
+
+  if (brew.profit > 0) {
+    console.log(`Brew profit: ${brew.profit.toFixed(2)} | sell3: ${brew.sell3.price} | buy4: ${brew.buy4.price}`);
+  }
+  if (restore.profit > 0) {
+    console.log(`Restore profit: ${restore.profit.toFixed(2)} | sell3: ${restore.sell3.price} | buy4: ${restore.buy4.price}`);
+  }
+  if (stamina.profit > 0) {
+    console.log(`Stamina profit: ${stamina.profit.toFixed(2)} | sell3: ${stamina.sell3.price} | buy4: ${stamina.buy4.price}`);
+  }
+}
+
+async function checkArbitrage(id4: number, id3: number, name: string) {
+  const buy4 = await getLastBuy(id4);
+  const sell3 = await getLastSell(id3);
+  const profit = buy4.price * 0.99 - (sell3.price / 0.75);
+  return {buy4, sell3, profit};
+}
